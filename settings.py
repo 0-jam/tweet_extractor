335b78c1d801818eeeab580da76f5bbbf748f5bc
0-jam <0jamaozm1137@gmail.com>
2019-06-03 17:52:37 +0900

    Removed unused environment variable

--- settings.py
+++ settings.py
@@ -4,12 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-TWITTER_ACCESS_TOKEN = os.environ.get('TWITTER_ACCESS_TOKEN')
-TWITTER_ACCESS_SECRET = os.environ.get('TWITTER_ACCESS_SECRET')
-
 TWITTER_API_KEY = os.environ.get('TWITTER_API_KEY')
 TWITTER_API_SECRET = os.environ.get('TWITTER_API_SECRET')
-
-TWITTER_BEARER_TOKEN = os.environ.get('TWITTER_BEARER_TOKEN')
 
 GMAPS_API_KEY = os.environ.get('GOOGLE_API_KEY')
