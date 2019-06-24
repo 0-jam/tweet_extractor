152c2e1e2fe7ea92d7698fc5b0be39c34c18ed00
0-jam <0jamaozm1137@gmail.com>
2019-06-24 12:01:37 +0900

    Changed some default output JSON file names

--- bulk_tweet_searcher.py
+++ bulk_tweet_searcher.py
@@ -12,7 +12,6 @@ def main():
     parser = argparse.ArgumentParser(description='Search trends in a specified location and collect tweets from trends')
     # Optional arguments
     parser.add_argument('--locale', type=str, help='Location name (default: None (collect global trends))')
-    parser.add_argument('--output', type=str, default='trending_tweets_{}.json'.format(datetime.today().strftime('%Y%m%d%H%M')), help='Output file path (default: trending_tweets_<YYYYMMDDHHMM>.json)')
     args = parser.parse_args()
 
     if args.locale:
@@ -21,11 +20,21 @@ def main():
         print('Location is not specified. Retrieving global trends ...')
         trends = twitter.search_trends_by_id()
 
-    trend_names = [trend['name'] for trend in trends[0]['trends']]
+    locale = trends['locations'][0]['name']
+    today = datetime.today().strftime('%Y%m%d%H%M')
 
+    # Save retrieved trends as the same format of trend_searcher.py
+    print('Saving trends...')
+    with Path('trends_{}_{}.json'.format(locale, today)).open('w', encoding='utf-8') as out:
+        out.write(json.dumps(trends, ensure_ascii=False, indent=2))
+
+    trend_names = [trend['name'] for trend in trends['trends']]
     tweets = {trend: twitter.search_tweet(trend) for trend in tqdm(trend_names, desc='Retrieving tweets...')}
 
-    with Path(args.output).open('w', encoding='utf-8') as out_json:
+    # Output tweets as the JSON file
+    # format: {topic: [tweets], topic2: [tweets], ...}
+    print('Saving tweets...')
+    with Path('tweets_{}_{}.json'.format(locale, today)).open('w', encoding='utf-8') as out_json:
         out_json.write(json.dumps(tweets, ensure_ascii=False, indent=2))
 
 
--- trend_searcher.py
+++ trend_searcher.py
@@ -1,5 +1,6 @@
 import argparse
 import json
+from datetime import datetime
 from pathlib import Path
 
 import modules.twitter as twitter
@@ -8,18 +9,23 @@ import modules.twitter as twitter
 def main():
     parser = argparse.ArgumentParser(description='Search trends in a specified location')
     # Optional arguments
-    parser.add_argument('--query', type=str, help='Location name (default: None (collect global trends))')
-    parser.add_argument('--output', type=str, default='trends.json', help='Output file path (default: results.json)')
+    parser.add_argument('--locale', type=str, help='Location name (default: None (collect global trends))')
+    parser.add_argument('--output', type=str, help='Output file path (default: trends_<locale>_<YYYYMMDDHHMM>.json)')
     args = parser.parse_args()
 
-    if args.query:
-        trends = twitter.search_trends_by_query(args.query)
+    if args.locale:
+        trends = twitter.search_trends_by_query(args.locale)
     else:
         print('Location is not specified. Retrieving global trends ...')
         trends = twitter.search_trends_by_id()
 
-    with Path(args.output).open('w', encoding='utf-8') as out:
-        out.write(json.dumps(trends[0], ensure_ascii=False, indent=4))
+    if args.output:
+        outpath = Path(args.output)
+    else:
+        outpath = Path('trends_{}_{}.json'.format(trends['locations'][0]['name'], datetime.today().strftime('%Y%m%d%H%M')))
+
+    with outpath.open('w', encoding='utf-8') as out:
+        out.write(json.dumps(trends, ensure_ascii=False, indent=2))
 
 
 if __name__ == "__main__":
